78c2e9e41d85ac9fbedcac0f685b4ddbc6c90eca
Arunkumar Selvaraj <arun@ioak.io>
2022-12-15 16:18:23 +0530

    deployment changes

--- src/lib/minioutils.ts
+++ src/lib/minioutils.ts
@@ -46,7 +46,7 @@ const toBase64 = (file: any) => new Promise((resolve, reject) => {
   const reader = new FileReader();
   reader.readAsDataURL(file);
   reader.onload = () => resolve(reader.result);
-  reader.onerror = (error) => reject(error);
+  reader.onerror = (error: any) => reject(error);
 });
 
 export const processFileUploadBkp = async (
