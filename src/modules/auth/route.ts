416db709db1d8afbdb64ee0c95dc29653e6fedd2
Arunkumar Selvaraj <arun@ioak.io>
2022-12-28 15:56:51 +0530

    Added a test service

--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -9,5 +9,6 @@ const selfRealm = 100;
 module.exports = function (router: any) {
   router.post("/auth/signin", asyncHandler(signin));
   router.post("/otrs", asyncHandler(otrs));
+  router.post("/otrs/ticket/_doc/:ticketId", asyncHandler(otrs));
   router.post("/auth/changepassword", authorizeApi, asyncHandler(changepassword));
 };
